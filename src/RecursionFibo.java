1f3f51b0936d1be456ab51176aba07b87594fd69
shubhs06 <shamacharekar48@gmail.com>
2025-07-27 18:49:56 +0530

    Added Recursion Programs

--- src/RecursionFibo.java
+++ src/RecursionFibo.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class RecursionFibo {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the number : ");
-    int n = sc.nextInt();
-    for (int i = 1; i <= n; i++) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
         System.out.print(fibo(i) + " ") ;        
-    }
+        }
 
-}
-public static int fibo(int n){
-    
-    if (n == 1) {
-        return 0;
     }
-    if (n == 2) {
-        return 1;
-    }
+    public static int fibo(int n){
     
-    return fibo(n -1) + fibo(n - 2) ;
-}
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+    
+        return fibo(n -1) + fibo(n - 2) ;
+    }
 
 }
